db5bc9e9ace44748c043edf0dc33b58fb32d61b6
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-07-27 15:05:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+OWNER_ID = int(getenv("OWNER_ID", ""))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "STYLISH_FONT")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "DHPR_SUPPORT")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "")
 
 # Random Start Images
 IMG = [
